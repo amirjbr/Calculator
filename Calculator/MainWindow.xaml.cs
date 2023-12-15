7cc049c83e9d138468ca615f51a27e7a58ad8a55
amirjbr <153921915+amirjbr@users.noreply.github.com>
2023-12-15 16:56:02 +0330

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double lastNumber, result;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            actionAcButton.Click += ActionAcButton_Click;
+            actionNegativeButton.Click += ActionNegativeButton_Click;
+            actionPercentageButton.Click += ActionPercentageButton_Click;
+            actionEqualButton.Click += ActionEqualButton_Click;
+        }
+
+        private void ActionEqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ActionPercentageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void ActionNegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse( resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void ActionAcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void numberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = int.Parse((sender as Button).Content.ToString());
+          
+            if (resultLabel.Content.ToString() == "0")
+                resultLabel.Content = selectedValue.ToString();
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            }
+        }
+        
+    }
+}
